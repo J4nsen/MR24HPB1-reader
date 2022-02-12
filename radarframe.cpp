b60442db1b348ebfcee22bfb855a49e46fc09279
Jan Wiele <jan@wiele.org>
2022-02-12 16:37:13 +0100

    show motor data as float

--- radarframe.cpp
+++ radarframe.cpp
@@ -1,6 +1,7 @@
 #include "radarframe.h"
 #include <QDebug>
 #include <QVariant>
+#include <QByteArray>
 
 RadarFrame::RadarFrame(QByteArray raw_frame, QObject *parent)
     : QObject{parent}
@@ -13,6 +14,13 @@ RadarFrame::RadarFrame(QByteArray raw_frame, QObject *parent)
     unsigned char addressCode1 = raw_frame.at(4);
     unsigned char addressCode2 = raw_frame.at(5);
     m_data = raw_frame.mid(6, frame_size-7);
+
+    m_crcLow = raw_frame.at(raw_frame.size()-2);
+    m_crcHigh = raw_frame.at(raw_frame.size()-1);
+
+//    qDebug() << QByteArray(1, m_crcLow).toHex();
+//    qDebug() << QByteArray(1, m_crcHigh).toHex();
+
 
     switch(functionCode){
     case 1:
@@ -59,8 +67,32 @@ RadarFrame::RadarFrame(QByteArray raw_frame, QObject *parent)
 
         break;
     }
-    qDebug() << m_functionCode;
-    qDebug() << m_addressCodes1;
-    qDebug() << m_addressCodes2;
-    qDebug() << m_data.toHex();
+    //qDebug() << m_functionCode;
+    //qDebug() << m_addressCodes1;
+    //qDebug() << m_addressCodes2;
+    //qDebug() << m_data.toHex();
+
+    if(m_functionCode == RadarFrame::PROACTIVE_REPORTING && m_addressCodes1 == RadarFrame::REPORT_RADAR_INFO && m_addressCodes2 == RadarFrame::MOTOR_SIGNS){
+        Float_Byte fb;
+        fb.Byte[0] = m_data[0];
+        fb.Byte[1] = m_data[1];
+        fb.Byte[2] = m_data[2];
+        fb.Byte[3] = m_data[3];
+        qDebug() << "Motor" << fb.Float;
+    }
 }
+
+
+//static unsigned short int us_CalculateCrc16(unsigned char *lpuc_Frame, unsigned short int lus_Len)
+//{
+//    unsigned char luc_CRCHi = 0xFF;
+//    unsigned char lu
+
+//    while(lus_Len--)
+//    {
+//        li_Index = luc_CRCLo ^ *( lpuc_Frame++);
+//        luc_CRCLo = (unsigned char)( luc_CRCHi ^ cuc_CRCHi[li_Index]);
+//        luc_CRCHi = cuc_CRCLo[li_Index];
+//    }
+//    return (unsigned short int )(luc_CRCLo << 8 | luc_CRCHi);
+//}
